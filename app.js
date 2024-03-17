55f3e0a262dc6be849d2aee81ef41caf20ecb00e
Ibrahim Bin Aboobacker CH <91882961+Ibnuibaf@users.noreply.github.com>
2024-03-17 15:54:27 +0530

    completion: change port 10000

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use("/api/user/", userRouter);
 app.use("/api/product/", productRouter);
 app.use("/api/review/", reviewRouter);
 
-app.listen(3000, async () => {
+app.listen(10000, async () => {
   console.log("Server starting on http://localhost:10000");
   await connectDB();
   // await fetchAndStoreData()
